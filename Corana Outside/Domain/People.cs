e05ff5a0a4aa382773a895c4b333e0bbfe002638
Ivan Gladko <gladkott@gmail.com>
2020-04-28 07:57:53 +0700

    Lab 6

---
+++ Corana Outside/Domain/People.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Domain
+{
+    public enum Surname
+    {
+        Бонд,
+        Кардашьян,
+        Рокет,
+        Фреско,
+        Маск
+    }
+    public class People
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Surname surname { get; set; }
+        string statusAge = "лет";
+        public override string ToString()
+        {
+            return Name + surname +  " " + Age + " " + statusAge;
+        }
+        
+    }
+}
